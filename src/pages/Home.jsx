51966f02fc806cdcdf067b996d9e0426dc85cafe
lomba1992 <lomba.nicolo@gmail.com>
2019-06-03 16:36:26 +0200

    Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,12 +16,28 @@ class Home extends React.Component{
                 <header>
                     <h2>Home</h2>
                 </header>
-                <section className="container-home">
-                    <main className="real-container">
+                <main className="container-home">
+                    <section className="real-container">
+                        <section className="summary">
+                            <img src="#" alt="img_students" width="120" height="120"/>
+                            <p>Nome Studente</p>
+                            <p>Corso Studente</p>
+                            <sectione className="summary-lessons">
+                                <div className="circle"></div>
+                                <div className="circle"></div>
+                            </sectione>
+                        </section>
+                        <section className="summary-rewards">
+                            <div className="achievement"></div>
+                            <div className="achievement"></div>
+                            <div className="achievement"></div>
+                        </section>
+                        <section className="summary-lessonDay">
 
-                    </main>
+                        </section>
+                    </section>
                         
-                </section>
+                </main>
             </>
         );
             
